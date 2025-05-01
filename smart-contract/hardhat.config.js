25541da0e772264967c9d17ec3def9571ab711b9
Imam ariadi <gamerapin139@gmail.com>
2025-05-01 16:37:46 +0700

    malah eror semua wkwk

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,29 +1,12 @@
-// require("@nomiclabs/hardhat-ethers");
-// require("dotenv").config();
-
-// module.exports = {
-//   solidity: "0.8.0",
-//   networks: {
-//     sepolia: {
-//       url: `https://eth-sepolia.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-//       accounts: [`0x${process.env.PRIVATE_KEY}`], // Gunakan private key untuk wallet
-//     },
-//   },
-// };
-
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    hardhat: {
-      chainId: 1337
-    },
     localhost: {
       url: "http://127.0.0.1:8545",
-      accounts: [/* private key account */]
+      accounts: { mnemonic: process.env.MNEMONIC }
     }
   }
 };
-
-
--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,8 +1,13 @@
-const { ethers } = require("hardhat");
+const hre = require("hardhat");
 
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
+
+  if (!deployer) {
+    console.error("Deployer is undefined, check the configuration and network.");
+    return;
+  }
 
   const Todo = await ethers.getContractFactory("Todo");
   const todo = await Todo.deploy();
